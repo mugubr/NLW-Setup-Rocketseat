69f8cb5ad4c9a2202ff8ab98049204cdfa3281e7
mugubr <miguimuniz@gmail.com>
2023-01-24 10:35:10 -0300

    wip: backend/frontend (web)

--- NLW Setup/web/tailwind.config.cjs
+++ NLW Setup/web/tailwind.config.cjs
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx',
-    './index.html'
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A',
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      },
+      gridTemplateRows:{
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
